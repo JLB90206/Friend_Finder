1cd1c9fbc92226cdd5556554068dad376042be05
Jordan Babbitt <JBabbitt27@gmail.com>
2019-04-10 21:47:09 -0700

    Final Working Upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Grace",
+		photo: "https://vignette.wikia.nocookie.net/willgrace/images/9/9d/Profile_grace.jpg/revision/latest?cb=20161128024140",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		name: "Karen",
+		photo: "https://www.triviatribute.com/images4/meganmullally1a.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		name: "Jack",
+		photo: "https://vignette.wikia.nocookie.net/willgrace/images/4/47/Sean_Hayes_as_Jack.jpg/revision/latest?cb=20160914055444",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		name: "Will",
+		photo: "http://images2.fanpop.com/image/photos/10800000/will-and-grace-will-and-grace-10880559-264-400.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		name: "Lorraine Finster",
+		photo: "https://vignette.wikia.nocookie.net/willgrace/images/e/e0/Screen_Shot_2017-11-29_at_22.01.17.png/revision/latest?cb=20180314070053",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
